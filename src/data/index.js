882d7409d4c409ce700771e9b65d8638d7487c89
Sandra Vargas <sandra@Sandras-MacBook-Pro.local>
2021-07-06 13:51:01 -0700

    Add dummy data

---
+++ src/data/index.js
@@ -0,0 +1,144 @@
+export const projects = [
+  {
+    id: '001',
+    title: 'React Reserve',
+    subtitle: 'MERN Stack',
+    description: {
+      short:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    },
+    image: {
+      thumbnail: './project-thumb.jpg',
+      full: './project.jpg',
+    },
+    url: {
+      code: 'https://github.com/lifeanddoodles/',
+      live: '',
+    },
+    client: null,
+    creativeItemType: `WebSite`,
+    technologies: ['HTML', 'CSS'],
+    tags: ['Web Development', 'Theme Development'],
+  },
+  {
+    id: '002',
+    title: 'React Tracks',
+    subtitle: 'React and Python',
+    description: {
+      short:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    },
+    image: {
+      thumbnail: './project-thumb.jpg',
+      full: './project.jpg',
+    },
+    url: {
+      code: 'https://github.com/lifeanddoodles/',
+      live: '',
+    },
+    client: null,
+    creativeItemType: `WebSite`,
+    technologies: ['HTML', 'CSS'],
+    tags: ['Web Development', 'Theme Development'],
+  },
+  {
+    id: '003',
+    title: 'DevChat',
+    subtitle: 'React and Firebase',
+    description: {
+      short:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    },
+    image: {
+      thumbnail: './project-thumb.jpg',
+      full: './project.jpg',
+    },
+    url: {
+      code: 'https://github.com/lifeanddoodles/',
+      live: '',
+    },
+    client: null,
+    creativeItemType: `WebSite`,
+    technologies: ['HTML', 'CSS'],
+    tags: ['Web Development', 'Theme Development'],
+  },
+  {
+    id: '004',
+    title: 'Epic Todo App',
+    subtitle: 'React Hooks',
+    description: {
+      short:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    },
+    image: {
+      thumbnail: './project-thumb.jpg',
+      full: './project.jpg',
+    },
+    url: {
+      code: 'https://github.com/lifeanddoodles/',
+      live: '',
+    },
+    client: null,
+    creativeItemType: `WebSite`,
+    technologies: ['HTML', 'CSS'],
+    tags: ['Web Development', 'Theme Development'],
+  },
+]
+
+export const skills = [
+  {
+    icon: `html5`,
+    name: `HTML5`,
+    categories: ['frontend'],
+  },
+  {
+    icon: `css3`,
+    name: `CSS3`,
+    categories: ['frontend'],
+  },
+  {
+    icon: `sass`,
+    name: `Sass`,
+    categories: ['frontend'],
+  },
+  {
+    icon: `js`,
+    name: `JavaScript (ES6)`,
+    categories: ['frontend'],
+  },
+  {
+    icon: `react`,
+    name: `React`,
+    categories: ['frontend'],
+  },
+  {
+    icon: `images/logos/jquery.svg`,
+    name: `jQuery`,
+    categories: ['frontend'],
+  },
+  {
+    icon: `node`,
+    name: `Node.js`,
+    categories: ['backend'],
+  },
+  {
+    icon: `images/logos/express.svg`,
+    name: `Express.js`,
+    categories: ['backend'],
+  },
+  {
+    icon: `images/logos/photoshop.svg`,
+    name: `Photoshop`,
+    categories: ['design'],
+  },
+  {
+    icon: `images/logos/illustrator.svg`,
+    name: `Illustrator`,
+    categories: ['design'],
+  },
+  {
+    icon: `images/logos/indesign.svg`,
+    name: `InDesign`,
+    categories: ['design'],
+  },
+]
