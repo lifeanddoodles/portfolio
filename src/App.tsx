c3fb3b253a3c29f4cf4099860486804adc72e15e
Sandra Vargas <sandravargasgro@gmail.com>
2023-05-30 22:17:05 -0700

    Refactor App and Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
-// import './App.scss'
-import { Footer } from './components/Footer'
-import { Header } from './components/Header'
 import Home from './pages/Home'
 
 export const App = () => {
   return (
-    <>
-      <a className="skip-link screen-reader-text" href="#content">
-        Skip to content
-      </a>
-      <Header />
-      <div id="content" className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,27 @@
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
+import Hero from '../components/Hero'
 import About from './About'
 import Contact from './Contact'
-import Hero from '../components/Hero'
 import Projects from './Projects'
 
 const Home = () => {
   return (
-    <main>
-      <Hero />
-      <Projects />
-      <About />
-      <Contact />
-    </main>
+    <>
+      <a className="skip-link screen-reader-text" href="#content">
+        Skip to content
+      </a>
+      <Header />
+      <div id="content" className="content">
+        <main>
+          <Hero />
+          <Projects />
+          <About />
+          <Contact />
+        </main>
+      </div>
+      <Footer />
+    </>
   )
 }
 
